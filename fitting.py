f3c229f9d9c011ed74a1c8e550ff46a17a12df46
LuizFillip <luizfillip@outlook.com>
2025-08-04 15:30:38 -0300

    adjusting (draft)

--- fitting.py
+++ fitting.py
@@ -1,7 +1,49 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Aug  1 21:26:47 2025
+import PW as pw
+import datetime as dt 
+import matplotlib.pyplot as plt 
+import base as b 
 
-@author: Luiz
-"""
+freq = '15D'
+col = 'start'
+
+delta = dt.timedelta(days = 0)
+dn = dt.datetime(2013, 1, 1) + delta
+
+df = pw.epbs_start_time(dn, days = 50, reindex = False)
+
+df['mean'] = df[col].rolling(freq).mean()
+
+# df[col] = df[col] - df['mean']
+
+fig, ax = plt.subplots(
+    nrows = 2, 
+    # ncols = 2,
+    dpi = 300, 
+    # sharex = True,
+    figsize = (16, 8)
+    )
+
+df = df.dropna()
+color = '#0C5DA5'
+df['std'] = df[col].rolling(freq).std()
+
+x = df['doy'].values
+y = df[col].values
+ax[0].scatter(x, y) 
+
+ls = pw.Lomb_Scargle(x, y, Tmax = 20)
+
+periods, power = ls.result
+
+ax[1].plot(periods, power )
+
+best_T = ls.best_T 
+fit = b.CurveFit(x, y, period = 16)
+
+new_x, new_y = fit.get_values
+
+ax[0].plot(new_x, new_y)
+
+
+
 
